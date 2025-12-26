a3aae728fbcbec31298c2a9c94b97bc4ce765a9e
MALIK MUHAMMAD ZAHID <zaidiimalikofficial@gmail.com>
2025-12-26 10:46:06 +0500

    Add files via upload

---
+++ CA-02.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int arr[5];
+    arr[0]=24;
+    arr[1]=223;
+    arr[2]=543;
+    arr[3]=23;
+    arr[4]=4;
+
+    cout<<"Element at index 1 is "<<arr[1] << " is second element is array" <<endl;
+    cout<<"Element at index 3 is "<<arr[3] << " is fourth element is array" <<endl;
+}
